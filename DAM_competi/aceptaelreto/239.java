1a761c1a8bd9f23a0e027ef7ec218d00e9063a34
Yoon <yoonlopezluis@gmail.com>
2024-11-21 18:48:08 +0100

    Afegit 239

---
+++ DAM_competi/aceptaelreto/239.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class 239 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (!entrada.equals("0 0")) {
+            String entrada = scanner.nextString();
+
+            for (int i = 0; i < entrada.length(); i++) {
+                if (entrada.charAt(i) != ' ' && entrada.charAt(i) >= 0 && entrada.charAt(i) <= 9) {
+                    
+                }
+            }
+        }
+
+        scanner.close();
+    }
+}
